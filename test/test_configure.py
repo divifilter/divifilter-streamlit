40917884a9ebcd0433ef8506bae4b8dd369725cc
naorlivne <naorlivne@gmail.com>
2024-05-30 12:46:30 +0300

    unittest intial getting it to work push

--- test/test_configure.py
+++ test/test_configure.py
@@ -7,8 +7,7 @@ class TestReadConfigurations(unittest.TestCase):
     def test_read_configurations(self):
         # Test reading all configurations with default values
         config = read_configurations()
-        self.assertEqual(config["dividend_radar_download_url"], "https://www.portfolio-insight.com/dividend-radar")
-        self.assertEqual(config["local_file_path"], "/tmp/latest_dividend_radar.xlsx")
+        self.assertEqual(config["db_user"], "root")
 
     def test_read_configurations_missing_key(self):
         # Test missing configuration key
@@ -19,5 +18,6 @@ class TestReadConfigurations(unittest.TestCase):
     def test_read_configurations_default_values(self):
         # Test reading configurations with default values and some overridden values
         config = read_configurations(config_folder="tests/test_configs/default_values")
-        self.assertEqual(config["dividend_radar_download_url"], "https://www.portfolio-insight.com/dividend-radar")
-        self.assertEqual(config["local_file_path"], "/tmp/latest_dividend_radar.xlsx")
+        self.assertEqual(config["db_port"], 3306)
+        self.assertEqual(config["db_user"], "root")
+        self.assertEqual(config["db_schema"], "defaultdb")
